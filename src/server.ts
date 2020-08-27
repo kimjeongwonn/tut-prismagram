2a3cd18b02c2a556c7e1082d89f66ae046de3162
kimjeongwonn <66168187+kimjeongwonn@users.noreply.github.com>
2020-08-27 05:31:54 +0000

    setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 require('dotenv').config();
 import { GraphQLServer } from 'graphql-yoga';
-import logger from 'morgan';
+import morgan from 'morgan';
 import schema from './schema';
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
-server.express.use(logger('dev'));
+server.express.use(morgan('dev'));
 
 server.start({ port: PORT }, () => console.log(`Server running on port ${PORT}`));
